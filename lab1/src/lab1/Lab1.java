3872dd432a457201f6b5618cd83eb508d93bb5bc
Egor Trufanov <60848401+egortrufanov@users.noreply.github.com>
2023-12-18 08:07:51 +0300

    Add files via upload

---
+++ lab1/src/lab1/Lab1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author egortrufanov
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 5;
+        int d = 7;
+        int sum = a + d;
+        System.out.println("Sum = " + sum);
+    }
+    
+}
